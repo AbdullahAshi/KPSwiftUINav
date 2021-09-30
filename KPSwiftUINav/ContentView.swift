d3a3c923c65fc039d6f6007613028d636751eed2
Abdullah Al-Ashi <abdullah.alashi@gmail.com>
2021-09-30 14:58:37 +1000

    - create new SwiftUI project

---
+++ KPSwiftUINav/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KPSwiftUINav
+//
+//  Created by Abdullah Al-Ashi on 30/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
