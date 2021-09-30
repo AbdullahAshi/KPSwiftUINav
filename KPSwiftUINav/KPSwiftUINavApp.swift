802635df4b7d1c182d9e0ca529853dea5e110ce8
Abdullah Al-Ashi <abdullah.alashi@gmail.com>
2021-09-30 17:00:16 +1000

    - switch to @main App protocol

---
+++ KPSwiftUINav/KPSwiftUINavApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPSwiftUINavApp.swift
+//  KPSwiftUINav
+//
+//  Created by Abdullah Al-Ashi on 30/9/21.
+//
+
+import SwiftUI
+
+@main
+struct KPSwiftUINavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
